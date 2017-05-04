4a37623761fd88eb8d41f1129ea4fb51221ec3fc
SalVN <sal.v.newell@gmail.com>
2017-05-04 14:17:33 +0100

    basic game working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,59 @@
 $(document).ready(function (){
-    $('h1').click(function () {
-        console.log('clicked');
+    $('.wrong-msg, .correct-msg').hide();
+    var $wordArr = ['blizzard', 'crypt', 'bagpipes', 'fjord', 'beekeeper', 'waltz', 'xylophone', 'quorum', 'puppy', 'zombie', 'yipee', 'sphinx', 'lucky', 'kayak', 'peekaboo', 'rickshaw', 'zodiac', 'haiku', 'galaxy', 'ivory', 'abyss', 'phlegm', 'pneumonia', 'injury'];
+ //   console.log($wordArr.length);
+
+//generate a random word from the array (use array.length to allow more words to be added later)
+    var $wordSelector = $wordArr[Math.floor(Math.random()*$wordArr.length)];
+    console.log($wordSelector);
+
+//print lines on the screen for each letter
+    var $screenWord = $wordSelector.replace(/[a-z+]/g, '__ .' ).split('.');
+       console.log($screenWord);
+    $('.word-to-guess').append($screenWord);
+    console.log($screenWord);
+
+var $index;
+//prompt a guess
+var $clickCount = 0;
+    $('.guess-button').click(function (){
+        $clickCount++;
+        console.log($clickCount);
+        if ($clickCount > 1) {
+            var $oldResult = $index;
+            console.log($oldResult);
+        } 
+
+
+        var $guess = prompt('Guess a Letter').toLowerCase();
+        
+        console.log($guess);
+        
+        var $searchable = $wordSelector.split('');
+        console.log($searchable);
+        
+
+        $index =[];
+         for (var i = 0; i < $searchable.length; i++) {
+            if ($searchable[i]===$guess) {
+                $index.push($guess  + " ");
+            } else {
+                if ($clickCount === 1) {
+                    $index.push($screenWord[i]);
+                } else {
+                    $index.push($oldResult[i]);
+                }
+            }    
+         }
+        console.log($index);
+        return $('.word-to-guess').html($index);
+        
+        
+        
+
     });
+    
+
+
+
 });
