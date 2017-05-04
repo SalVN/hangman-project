39cea409c57ee173ca85714d84643391c8d77bc4
SalVN <sal.v.newell@gmail.com>
2017-05-04 14:47:09 +0100

    added messages for correct and incorrect

--- js/main.js
+++ js/main.js
@@ -14,43 +14,53 @@ $(document).ready(function (){
     console.log($screenWord);
 
 var $index;
-//prompt a guess
+//prompt a guess function
 var $clickCount = 0;
     $('.guess-button').click(function (){
         $clickCount++;
         console.log($clickCount);
-        if ($clickCount > 1) {
-            var $oldResult = $index;
-            console.log($oldResult);
-        } 
-
-
+            if ($clickCount > 1) {
+                var $oldResult = $index;
+                console.log($oldResult);
+            } 
+        
         var $guess = prompt('Guess a Letter').toLowerCase();
         
-        console.log($guess);
+        //console.log($guess);
         
         var $searchable = $wordSelector.split('');
-        console.log($searchable);
+        //console.log($searchable);
         
-
         $index =[];
+        
+        var $changed = false;
          for (var i = 0; i < $searchable.length; i++) {
             if ($searchable[i]===$guess) {
                 $index.push($guess  + " ");
+                    $changed = true;
             } else {
                 if ($clickCount === 1) {
                     $index.push($screenWord[i]);
+
                 } else {
                     $index.push($oldResult[i]);
+  
                 }
             }    
          }
+         console.log($changed);
+        if ($changed === true) {
+            $('.correct-msg').show();
+            $('.wrong-msg').hide();
+        } else {
+            $('.wrong-msg').show();
+            $('.correct-msg').hide();
+        }
+
         console.log($index);
+        $('.guess-button').html('Guess Another Letter');
         return $('.word-to-guess').html($index);
         
-        
-        
-
     });
     
 
